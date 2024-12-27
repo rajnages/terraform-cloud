d43f5dcc9b7e01157b9b2958b831a3a0fff68c61
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2024-12-27 11:22:04 +0530

    ############

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  cloud {
+    organization = "Main-123"
+
+    workspaces {
+      name = "terraform-cloud"
+    }
+  }
+
+  required_version = ">= 1.1.2"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -7,15 +19,16 @@ resource "aws_vpc" "terraform_vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name = "terraform_vpc"
+    Name        = "terraform_vpc"
+    Environment = "Development"
   }
 }
-
 
 resource "aws_internet_gateway" "terraform_igw" {
   vpc_id = aws_vpc.terraform_vpc.id
   tags = {
-    Name = "terraform_igw"
+    Name        = "terraform_igw"
+    Environment = "Development"
   }
 }
 
@@ -23,14 +36,17 @@ resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.terraform_vpc.id
   cidr_block = "10.0.1.0/24"
   tags = {
-    Name = "public_subnet"
+    Name        = "public_subnet"
+    Environment = "Development"
   }
 }
-resource "aws_subnet" "Private_subnet" {
+
+resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.terraform_vpc.id
   cidr_block = "10.0.2.0/24"
   tags = {
-    Name = "Private_subnet"
+    Name        = "private_subnet"
+    Environment = "Development"
   }
 }
 
@@ -41,13 +57,16 @@ resource "aws_route_table" "public_rt" {
     gateway_id = aws_internet_gateway.terraform_igw.id
   }
   tags = {
-    Name = "public_rt"
+    Name        = "public_rt"
+    Environment = "Development"
   }
 }
+
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.terraform_vpc.id
   tags = {
-    Name = "private_rt"
+    Name        = "private_rt"
+    Environment = "Development"
   }
 }
 
@@ -55,7 +74,8 @@ resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
-resource "aws_route_table_association" "public_subnet_association" {
+
+resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_rt.id
 }
